8cad7eacdd45c3d4fa11630b25594844db5ab083
vadim <vad-gab9@yandex.ru>
2026-01-06 01:10:19 +0500

    fute: add middleware that check jwt

---
+++ go.mod
@@ -0,0 +1,7 @@
+module gateway
+
+go 1.25
+
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
+)
--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -1,1 +1,44 @@
 package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type contextKey string
+
+const UserIDKey contextKey = "userID"
+
+func Middleware(secret []byte, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		auth := r.Header.Get("Authorization")
+		if auth == "" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		parts := strings.SplitN(auth, " ", 2)
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := jwt.Parse(parts[1], func(t *jwt.Token) (interface{}, error) {
+			return secret, nil
+		})
+		if err != nil || !token.Valid {
+			http.Error(w, "invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		claims := token.Claims.(jwt.MapClaims)
+		userID := int64(claims["user_id"].(float64))
+
+		ctx := context.WithValue(r.Context(), UserIDKey, userID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
