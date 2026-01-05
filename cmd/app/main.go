7b720cbbd2107276663465a5544f1d9142df3b09
vadim <vad-gab9@yandex.ru>
2026-01-06 01:12:05 +0500

    fute: add new main func, where use router

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"gateway/internal/config"
+	"gateway/internal/router"
+)
+
+func main() {
+	cfg := config.Load()
+
+	handler := router.New(
+		cfg.JWTSecret,
+		cfg.UserAPIURL,
+		cfg.CatalogURL,
+		cfg.OrdersURL,
+	)
+
+	log.Println("gateway listening on :" + cfg.Port)
+	log.Fatal(http.ListenAndServe(":"+cfg.Port, handler))
+}
